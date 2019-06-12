2fc7e62c720bb930b42717d113d0f0892cda8225
LeChatong <ulrich.tchatong@gmail.com>
2019-06-12 16:18:34 +0100

    Amélioration de la recherche

--- OnlyMe/settings.py
+++ OnlyMe/settings.py
@@ -155,7 +155,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
