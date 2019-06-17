86b079afd9835f257e58cf53efa6bb4f2f4d4fec
LeChatong <ulrich.tchatong@gmail.com>
2019-06-17 18:22:21 +0100

    Ajout du du téléchargement et de nouvelles tables dans la BD

--- MoviesAndMe/models.py
+++ MoviesAndMe/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class movie_detail(models.Model):
     id_movie = models.IntegerField()
-    title_movie = models.CharField(max_length=250)
-    link_telegram = models.URLField()
+    title_movie = models.CharField(verbose_name='Titre',max_length=250)
+    link_telegram = models.URLField(verbose_name='Lien Télégram')
     LANGUAGE = (
         ('FRANÇAIS', 'FRANÇAIS'),
         ('ANGLAIS', 'ANGLAIS'),
@@ -22,23 +22,24 @@ class movie_detail(models.Model):
         ('NON', 'NON'),
     )
 
-    voice_language = models.CharField(max_length=10, choices=LANGUAGE)
-    quality_video = models.CharField(max_length=10, choices=QUALITY)
-    quality_audio = models.CharField(max_length=10, choices=QUALITY)
-    subtitle = models.CharField(max_length=3, choices=YES_OR_NO)
-    subtitle_language = models.CharField(max_length=10, choices=LANGUAGE)
+    voice_language = models.CharField(verbose_name='Langue',max_length=10, choices=LANGUAGE)
+    quality_video = models.CharField(verbose_name='Qualité Vidéo',max_length=10, choices=QUALITY)
+    quality_audio = models.CharField(verbose_name='Qualité Audio',max_length=10, choices=QUALITY)
+    subtitle = models.CharField(verbose_name='Soutitré ?',max_length=3, choices=YES_OR_NO)
+    subtitle_language = models.CharField(verbose_name='Langue du Sous-titre',max_length=10, choices=LANGUAGE)
     class Meta:
-        verbose_name    = 'movie_detail'
+        verbose_name    = 'Film'
+        verbose_name_plural = 'Films'
         ordering        = ['title_movie']
     def __str__(self):
         return self.title_movie
 
 class tv_detail(models.Model):
     id_tv = models.IntegerField()
-    nb_season = models.IntegerField(default=0)
-    nb_episode = models.IntegerField(default=0)
-    title_tv = models.CharField(max_length=250)
-    link_telegram = models.URLField()
+    nb_season = models.IntegerField(verbose_name='Saison',default=0)
+    nb_episode = models.IntegerField(verbose_name='Episode',default=0)
+    title_tv = models.CharField(verbose_name='Titre',max_length=250)
+    link_telegram = models.URLField(verbose_name='Lien Télégram')
     LANGUAGE = (
         ('FRANÇAIS', 'FRANÇAIS'),
         ('ANGLAIS', 'ANGLAIS'),
@@ -56,13 +57,14 @@ class tv_detail(models.Model):
         ('NON', 'NON'),
     )
 
-    voice_language = models.CharField(max_length=10, choices=LANGUAGE)
-    quality_video = models.CharField(max_length=10, choices=QUALITY)
-    quality_audio = models.CharField(max_length=10, choices=QUALITY)
-    subtitle = models.CharField(max_length=3, choices=YES_OR_NO)
-    subtitle_language = models.CharField(max_length=10, choices=LANGUAGE)
+    voice_language = models.CharField(verbose_name='Langue', max_length=10, choices=LANGUAGE)
+    quality_video = models.CharField(verbose_name='Qualité Vidéo',max_length=10, choices=QUALITY)
+    quality_audio = models.CharField(verbose_name='Qualité Audio',max_length=10, choices=QUALITY)
+    subtitle = models.CharField(verbose_name='Soutitré ?',max_length=3, choices=YES_OR_NO)
+    subtitle_language = models.CharField(verbose_name='Langue du Sous-titre',max_length=10, choices=LANGUAGE)
     class Meta:
-        verbose_name    = 'tv_detail'
+        verbose_name    = 'Série/Animé'
+        verbose_name_plural = 'Séries/Animés'
         ordering        = ['title_tv']
     def __str__(self):
         return self.title_tv
--- MoviesAndMe/tests.py
+++ MoviesAndMe/tests.py
@@ -1,3 +1,8 @@
+from django.urls import reverse
 from django.test import TestCase
 
 # Create your tests here.
+class HomePageTestCase(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
