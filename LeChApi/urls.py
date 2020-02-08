6596742bbe8e7713eb68334a2855e6e769d2129b
LeChatong <ulrich.tchatong@gmail.com>
2020-02-08 14:52:26 +0100

    Début d'ajout de l'API + Partage Telegram, Whatsapp

--- LeChApi/urls.py
+++ LeChApi/urls.py
@@ -4,4 +4,6 @@ from LeChApi import views
 
 urlpatterns = (
     url(r'^movies/$', views.list_all_movies_avaible),
+    url(r'^auth/$', views.authentification),
+    url(r'^uuser_data', views.user_data, name='user_data'),
 )
--- LeChApi/views.py
+++ LeChApi/views.py
@@ -11,3 +11,17 @@ def list_all_movies_avaible(request):
     data = movie_detail.objects.all()
     serializer = MovieSerializer(data, context={'request': request}, many=True)
     return Response(serializer.data)
+
+def authentification(request):
+    return render(request, 'auth.html')
+
+def user_data(resquest):
+    if resquest.method == 'GET':
+        id = resquest.POST['id']
+        first_name = resquest.POST['first_name']
+        last_name = resquest.POST['last_name']
+        username = resquest.POST['username']
+        photo_url = resquest.POST['photo_url']
+        auth_date = resquest.POST['auth_date']
+        hash = resquest.POST['hash']
+        return render(resquest, 'user_data.html', locals())
